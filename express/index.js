a6e0f874443a9884bd62d3e2e8cd21c4b7c7db50
Karan Singh <karan1205singh@gmail.com>
2021-05-16 18:39:11 +0530

    express app

---
+++ express/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.end();
+});
+
+app.listen(PORT, () => {
+    console.log(`App is up on ${process.env.PORT || PORT}`);
+});
+
+module.exports.handler = serverless(app);
