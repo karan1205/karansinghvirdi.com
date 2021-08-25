d708945a9b2f72091e4c41cf1ab2833ddd01d4cc
Karan Singh <karan1205singh@gmail.com>
2021-08-25 16:31:48 +0530

    video player testing

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,12 @@ const App = () => {
                          height="526"
                          stretching="responsive"
             />
+            <VideoPlayer id="knkdsnfvvsvsdndksfds"
+                          srcUrl="https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8"
+                         width="852"
+                         height="526"
+                         stretching="responsive"
+            />
         </Fragment>
     )
 };
